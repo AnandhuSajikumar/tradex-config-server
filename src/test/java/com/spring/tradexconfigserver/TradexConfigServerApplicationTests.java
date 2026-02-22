1608a7bff5eb4661d1458f810274fb195df1969a
anandhu <anandhuzz916@gmail.com>
2026-02-22 19:45:29 +0530

    Initial commit

---
+++ src/test/java/com/spring/tradexconfigserver/TradexConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tradexconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradexConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
